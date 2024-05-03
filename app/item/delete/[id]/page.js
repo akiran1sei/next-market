8286f8cfe8f2494841d0883ff421f577d3648dd3
akiran1sei <nakamori.1224@gmail.com>
2024-05-03 15:44:06 +0900

    import dotenv from "dotenv";

--- app/item/delete/[id]/page.js
+++ app/item/delete/[id]/page.js
@@ -1,74 +1,86 @@
-"use client"
-import { useState, useEffect } from "react" 
-import Image from "next/image" 
-import useAuth from "../../../utils/useAuth" 
-
+"use client";
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import useAuth from "../../../utils/useAuth";
+import dotenv from "dotenv";
 const DeleteItem = (context) => {
-    const [title, setTitle] = useState("")
-    const [price, setPrice] = useState("")
-    const [image, setImage] = useState("")
-    const [description, setDescription] = useState("")
-    const [email, setEmail] = useState("")
-    const [loading, setLoading] = useState(false)
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState("");
+  const [description, setDescription] = useState("");
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+  dotenv.config();
+  const loginUserEmail = useAuth();
 
-    const loginUserEmail = useAuth()
+  useEffect(() => {
+    const getSingleItem = async (id) => {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_URL}/api/item/readsingle/${id}`,
+        { cache: "no-store" }
+      );
+      const jsonData = await response.json();
+      const singleItem = jsonData.singleItem;
+      setTitle(singleItem.title);
+      setPrice(singleItem.price);
+      setImage(singleItem.image);
+      setDescription(singleItem.description);
+      setEmail(singleItem.email);
+      setLoading(true);
+    };
+    getSingleItem(context.params.id);
+  }, [context]);
 
-    useEffect(() => {
-        const getSingleItem = async(id) => {
-            const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/item/readsingle/${id}`, {cache: "no-store"}) 
-            const jsonData = await response.json() 
-            const singleItem = jsonData.singleItem         
-            setTitle(singleItem.title)
-            setPrice(singleItem.price)
-            setImage(singleItem.image)
-            setDescription(singleItem.description)
-            setEmail(singleItem.email)
-            setLoading(true) 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_URL}/api/item/delete/${context.params.id}`,
+        {
+          method: "DELETE",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+          body: JSON.stringify({
+            email: loginUserEmail,
+          }),
         }
-        getSingleItem(context.params.id) 
-    }, [context])  
+      );
+      const jsonData = await response.json();
+      alert(jsonData.message);
+    } catch (err) {
+      alert("アイテム削除失敗");
+    }
+  };
 
-    const handleSubmit = async(e) => {
-        e.preventDefault()  
-        try{
-            const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/item/delete/${context.params.id}`, {  
-                method: "DELETE",        
-                headers: { 
-                    "Accept": "application/json", 
-                    "Content-Type": "application/json",
-                    "Authorization": `Bearer ${localStorage.getItem("token")}`
-                },
-                body: JSON.stringify({
-                    email: loginUserEmail    
-                })
-            })
-            const jsonData = await response.json()  
-            alert(jsonData.message)           
-        }catch(err){
-            alert("アイテム削除失敗")  
-        }
-    } 
+  if (loading) {
+    if (loginUserEmail === email) {
+      return (
+        <div>
+          <h1 className="page-title">アイテム削除</h1>
+          <form onSubmit={handleSubmit}>
+            <h2>{title}</h2>
+            <Image
+              src={image}
+              width={750}
+              height={500}
+              alt="item-image"
+              priority
+            />
+            <h3>¥{price}</h3>
+            <p>{description}</p>
+            <button>削除</button>
+          </form>
+        </div>
+      );
+    } else {
+      return <h1>権限がありません</h1>;
+    }
+  } else {
+    return <h1>Loading...</h1>;
+  }
+};
 
-    if(loading){  
-        if(loginUserEmail === email){ 
-            return (
-                <div>
-                    <h1 className="page-title">アイテム削除</h1>
-                    <form onSubmit={handleSubmit}>
-                        <h2>{title}</h2>
-                        <Image src={image} width={750} height={500} alt="item-image" priority/>
-                        <h3>¥{price}</h3>
-                        <p>{description}</p>
-                        <button>削除</button>
-                    </form>
-                </div>
-            )
-        }else{  
-            return <h1>権限がありません</h1>
-        }
-    }else{                                    
-        return <h1>Loading...</h1>            
-    } 
-}
-
-export default DeleteItem
+export default DeleteItem;
--- app/item/readsingle/[id]/page.js
+++ app/item/readsingle/[id]/page.js
@@ -1,32 +1,42 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from "next/image";
+import Link from "next/link";
+import dotenv from "dotenv";
+const getSingleItem = async (id) => {
+  dotenv.config();
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_URL}/api/item/readsingle/${id}`,
+    { cache: "no-store" }
+  );
+  const jsonData = await response.json();
+  const singleItem = jsonData.singleItem;
+  return singleItem;
+};
 
-const getSingleItem = async(id) => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/item/readsingle/${id}`, {cache: "no-store"}) 
-    const jsonData = await response.json() 
-    const singleItem = jsonData.singleItem         
-    return singleItem 
-}
-
-const ReadSingleItem = async(context) => {                        
-    const singleItem = await getSingleItem(context.params.id)     
-    return (
-        <div className="grid-container-si">
-            <div>
-                <Image src={singleItem.image} width={750} height={500} alt="item-image" priority/>
-            </div>
-            <div>
-                <h1>{singleItem.title}</h1>
-                <h2>¥{singleItem.price}</h2>
-                <hr/>
-                <p>{singleItem.description}</p>
-                <div>
-                    <Link href={`/item/update/${singleItem._id}`}>アイテム編集</Link>
-                    <Link href={`/item/delete/${singleItem._id}`}>アイテム削除</Link>
-                </div>
-            </div>
+const ReadSingleItem = async (context) => {
+  const singleItem = await getSingleItem(context.params.id);
+  return (
+    <div className="grid-container-si">
+      <div>
+        <Image
+          src={singleItem.image}
+          width={750}
+          height={500}
+          alt="item-image"
+          priority
+        />
+      </div>
+      <div>
+        <h1>{singleItem.title}</h1>
+        <h2>¥{singleItem.price}</h2>
+        <hr />
+        <p>{singleItem.description}</p>
+        <div>
+          <Link href={`/item/update/${singleItem._id}`}>アイテム編集</Link>
+          <Link href={`/item/delete/${singleItem._id}`}>アイテム削除</Link>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default ReadSingleItem
+export default ReadSingleItem;
